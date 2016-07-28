d1e2a0bea4cec834f0618f8cbe98999c4524651a
fendouai <fendouai@fendouaideMacBook-Air.local>
2016-07-28 14:32:32 +0800

    fix the left nv

--- themes/readthedocs/js/jpush.js
+++ themes/readthedocs/js/jpush.js
@@ -1,56 +1,36 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-/*
-$(".subnavli").click(function(){
-    alert($(this).siblings().css("display"));
-    alert(!$(this).siblings().css("display")=="none");
-    if(!$(this).css("display")=="block")
-    {
-    var url = $(this).next().children().attr("href");
-    alert(url);
-    window.location.href=url;
-    $("li .current").parent().css("display","block");
-    $("li .current").parent().siblings().css("display","block");
-    $("li .current").parent().children().children("div").children("a").children("img").attr("src","/img/icon/down.png");
-    }
-    else
-    {
-    $("li .current").parent().children().children("div").children("a").children("img").attr("src","/img/icon/right.png");
-    $("li .current").parent().siblings().css("display","none");
-    }
-}
-);
-
-$(".subnavli").toggle(
-    function(){
-    var url = $(this).next().children().attr("href");
-    window.location.href=url;
-    },
-    function(){
-    var url = $(this).parent().parent().parent().children().first().children().children().children().children().attr("href");
-    window.location.href=url;
-    }
-);
-
-*/
-$(".subnavli").click(function(){
-    var url = $(this).next().children().attr("href");
-    window.location.href=url;
-});
+    $("li .current").parent().css("display", "block");
+    $("li .current").parent().siblings().css("display", "block");
+    $("li .current").parent().children(".subnavli").children().children("a").children().children("img").attr("src", "/img/icon/down.png");
 
 
-$("li .current").parent().css("display","block");
-$("li .current").parent().siblings().css("display","block");
-$("li .current").parent().children().children("div").children("a").children("img").attr("src","/img/icon/down.png");
+    $(".subnavul li").mouseover(function() {
+        $(this).css("background-color", "#eeeeee");
+        $("li .current").css("background-color", "#1d75bb");
+    });
+    $(".subnavul li").mouseout(function() {
+        $(this).css("background-color", "#f8f8f8");
+        $("li .current").css("background-color", "#1d75bb");
+    });
 
 
-$(".subnavul li").mouseover(function(){
-	$(this).css("background-color","#eeeeee");
-	$("li .current").css("background-color","#1d75bb");
-});
-$(".subnavul li").mouseout(function(){
-    $(this).css("background-color","#f8f8f8");
-    $("li .current").css("background-color","#1d75bb");
+    $(".subnavli").click(function() {
+        if ($(this).siblings().css("display") == "block") {
+            //alert("yes");
+            $(this).siblings().css("display","none");
+            $(this).children().children().children().children().attr("src", "/img/icon/right.png");
+            //$("li .current").parent().children().children("div").children("a").children("img").attr("src", "/img/icon/right.png");
+            //var url = $(this).parent().parent().parent().children().first().children().children().children().children().attr("href");
+            //alert(url);
+            //window.location.href = "http://192.168.9.74/jpush/";
+        } else {
+            var url = $(this).next().children().attr("href");
+            //alert("no");
+            window.location.href = url;
+            $("li .current").parent().css("display", "block");
+            $("li .current").parent().siblings().css("display", "block");
+            $("li .current").parent().children().children("div").children("a").children("img").attr("src", "/img/icon/down.png");
+        }
     });
 });
-
